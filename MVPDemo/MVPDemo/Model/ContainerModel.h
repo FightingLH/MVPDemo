36b394bbc68c95c4e7d69c00c0d7ea11195c598b
lh <feeyo@feeyodeMac-mini.local>
2017-11-24 09:05:49 +0800

    no message

---
+++ MVPDemo/MVPDemo/Model/ContainerModel.h
@@ -0,0 +1,13 @@
+//
+//  ContainerModel.h
+//  MVPDemo
+//
+//  Created by feeyo on 2017/11/23.
+//  Copyright © 2017年 lihuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContainerModel : NSObject
+
+@end
---
+++ MVPDemo/MVPDemo/Model/ContainerModel.m
@@ -0,0 +1,13 @@
+//
+//  ContainerModel.m
+//  MVPDemo
+//
+//  Created by feeyo on 2017/11/23.
+//  Copyright © 2017年 lihuan. All rights reserved.
+//
+//容器
+#import "ContainerModel.h"
+
+@implementation ContainerModel
+
+@end
